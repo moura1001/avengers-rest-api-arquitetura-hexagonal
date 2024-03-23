3bb0f77bbb11da21ea30978c6b55bd942e05ba06
moura1001 <gmouraneto01@gmail.com>
2024-03-23 08:55:46 -0300

    config: setup project

---
+++ src/main/java/com/moura/avengersapi/AvengersApiApplication.java
@@ -0,0 +1,13 @@
+package com.moura.avengersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvengersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvengersApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moura/avengersapi/AvengersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moura.avengersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvengersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
